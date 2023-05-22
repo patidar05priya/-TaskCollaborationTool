e396da67633bc53660c3f59e353be2fd77a61d6b
PRIYA PATIDAR <patidar05priya@gmail.com>
2023-05-22 14:34:19 -0400

    gitignore

---
+++ src/main/java/com/patidar/fitnessgoaltracker/FitnessGoalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.patidar.fitnessgoaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessGoalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessGoalTrackerApplication.class, args);
+	}
+
+}
