adb2ddf195c10ecd9d4dc16f645a2398fe87f46e
PRIYA PATIDAR <patidar05priya@gmail.com>
2023-05-22 14:45:31 -0400

    first commit

---
+++ src/test/java/com/patidar/fitnessgoaltracker/FitnessgoaltrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patidar.fitnessgoaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessgoaltrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
