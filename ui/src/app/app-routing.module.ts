088a932d1c7dc14f940e9a547c8a8293db1ce21d
PRIYA PATIDAR <patidar05priya@gmail.com>
2023-05-22 17:28:51 -0400

    so far workks

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }
 ];
---
+++ ui/src/app/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './welcome.component.html',
+})
+export class WelcomeComponent {
+
+
+  
+}
