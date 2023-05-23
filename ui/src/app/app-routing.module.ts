5b587409f110046343ea7eb9bb67eb7e043940e9
PRIYA PATIDAR <patidar05priya@gmail.com>
2023-05-23 14:19:58 -0400

    login

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -4,15 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProfileComponent } from './profile/profile.component';
-
-
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { UserService } from '../service/user.service';
 import { Router } from '@angular/router';
-
-import { User } from '../model/user.model';
+import { AuthGuard } from '../auth.guard';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,26 +12,33 @@ export class LoginComponent {
   emailControl = new FormControl('', [Validators.required, Validators.email]);
   passwordControl = new FormControl('', [Validators.required]);
   loggedIn : boolean;
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private userService: UserService, private router: Router, private authGuard: AuthGuard) {}
 
 
   login() {
+    this.authGuard.isActive = false;
     const email =  this.emailControl.value;
     const password =  this.passwordControl.value;
-
-
+  
     this.userService.getUserByEmail(email).subscribe(
       (user) => {
-        if(user.password == password )
-        console.log('User:', user);
-        this.loggedIn = true;
-        this.router.navigate(['/profile']); // Replace '/dashboard' with your desired route
-
+        if (user.password == password) {
+          console.log('User:', user);
+          this.authGuard.isActive = true;
+          this.router.navigate(['/profile']); // Navigate to the '/profile' route
+        } else {
+          console.log('Invalid password');
+          this.loggedIn = false;
+        }
       },
       (error) => {
         console.error('Error:', error);
         this.loggedIn = false;
       }
     );
+
+    this.authGuard.isActive = true;
+    this.router.navigate(['/profile']); // Navig
   }
+  
 }
